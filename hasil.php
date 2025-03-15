9ee2068aba5a187d404559d8fb0dbecc403c7464
Chrissatya Abel Cellonata <abelcellonata@gmail.com>
2025-03-15 20:25:38 +0700

    first commit

---
+++ hasil.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+        
+    </head>
+    <body>
+        <h1 style="font-weight: lighter;">Halaman hasil</h1>
+        <p>Setting yang tersedia: </p>
+        <textarea style="height: 150px; width: 450px;"></textarea>
+        <br><a href="setting.php" style="color: blue;">ganti setting</a>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+            Optio, vel dolor iure amet molestias consequuntur totam 
+            excepturi dicta a tempora earum aut dolorum officia porro 
+            eligendi hic! Expedita quibusdam, est atque corrupti consectetur 
+            quam sapiente libero animi quasi iste recusandae doloremque 
+            fugiat eos veritatis! Alias consequuntur cupiditate repellat 
+            inventore ducimus quos numquam. Laboriosam blanditiis perferendis 
+            vero nulla reiciendis? Laboriosam nobis ex, quisquam non eum 
+            dolor nihil soluta exercitationem ipsum iste earum libero excepturi 
+            deleniti ullam iure neque accusantium quae enim maiores pariatur 
+            velit eos sed? Saepe at in officiis ipsam cumque quod? Explicabo 
+            soluta quisquam, voluptas voluptates vitae consequatur repellendus 
+            temporibus ad obcaecati fugit facilis unde? Tenetur quia repudiandae 
+            ex vero soluta provident, exercitationem iste dolorum nobis atque 
+            necessitatibus maxime odit ad sit expedita id possimus perspiciatis 
+            molestias quasi aut ipsum magni fuga earum optio! Deserunt adipisci 
+            odio eveniet pariatur, earum porro quidem, at ullam, itaque inventore 
+            optio accusantium eligendi.
+        
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>index</title>
+        <style>
+            a {
+                color: black;
+                text-decoration: none;
+            }
+        </style>
+    </head>
+    <body>
+       <button><a href="hasil.php">Lihat Hasil</a></button>
+       <button><a href="setting.php">Lihat Setting</a></button>
+    </body>
+</html>
